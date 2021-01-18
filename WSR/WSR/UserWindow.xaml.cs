9b1d734899aa3c9dd391987b1edc256134d30f1d
ggwplarin <39963694+ggwplarin@users.noreply.github.com>
2021-01-18 20:51:56 +0300

    Add UserWindow files

---
+++ WSR/WSR/UserWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WSR
+{
+    /// <summary>
+    /// Логика взаимодействия для UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        private readonly Window _previousWindow;
+        public UserWindow(Window previousWindow, object args)
+        {
+            _previousWindow = previousWindow;
+            InitializeComponent();
+        }
+
+        private void BtnExit_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UserWindow_OnClosed(object? sender, EventArgs e)
+        {
+            _previousWindow.Show();
+        }
+    }
+}
